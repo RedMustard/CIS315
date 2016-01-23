a6e64c16c1b1bda283f362fe808ba179b4dd9474
Travis Barnes <ttb@uoregon.edu>
2016-01-23 12:29:12 -0800

    Initial Commit

---
+++ assignment2/paths.py
@@ -0,0 +1,22 @@
+"""Assignment 2
+
+Author: Travis Barnes, 23 January 2016
+
+Finds the longest, shortest, and number of paths, given a file of nodes.
+"""
+
+import sys.os
+
+def get_filename():
+	"""Requests user for file."""
+
+	filename = input("Enter the name of your new address book: ")
+
+
+
+def file_parse(filename):
+	"""Takes user specified file and parses it.
+
+	Keyword arguments:
+	filename -- Name of user specified file
+	"""
