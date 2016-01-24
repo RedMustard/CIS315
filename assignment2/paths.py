23480c6f159feb70bb0d590f7f87fef35fa60806
Travis Barnes <ttb@uoregon.edu>
2016-01-24 14:16:51 -0800

    Code cleanup and comments

--- assignment2/paths.py
+++ assignment2/paths.py
@@ -8,16 +8,20 @@ Finds the longest, shortest, and number of paths, given a file of nodes.
 import sys
 sys.path.insert(0, 'files')
 
-# COUNT = 0
 
 def file_parse():
-	"""Processes graph structures from user specified file."""
+	"""Processes graph structures from user specified file.
+
+	Returns:
+	graph_create() -- List of graphs
+	"""
 
 	filename = input("Enter the file path for your graph: ")
 	target = open(filename, 'r')
 
 	target_lines = [] 	# List of lines from target file
 	
+	# Grab the graph count and node/edge count for the first graph
 	i = 0
 	for line in target:
 		if i == 0:
@@ -33,25 +37,25 @@ def file_parse():
 	return graph_create(target_lines, graph_count, node_count, edge_count)
 
 
-def graph_create(line_list, graphs, nodes, edges):
-	"""Creates graphs from parsed file.
+def graph_create(line_list, graph_count, node_count, edge_count):
+	"""Create graphs from file_
 
 	Keyword arguments:
 	line_list 	-- a list of parsed file lines
-	graphs 	-- total number of graphs
-	nodes 	-- total number of nodes
-	edges 	-- total number of edges
+	graph_count -- total number of graphs
+	node_count 	-- total number of node
+	edge_count 	-- total number of edge
+
+	Returns:
+	graphs -- List of graphs
 	"""
-	graph_count = graphs
-	node_count = nodes
-	edge_count = edges
+
 	lines = line_list
 	graphs = []
 	x = 0
 
+	# For each graph
 	for i in range(graph_count):
-		print("Graph number: {}".format(i+1))
-
 		prev_node = lines[0].split()[0]
 		edges = []
 		graph = []
@@ -60,9 +64,9 @@ def graph_create(line_list, graphs, nodes, edges):
 		longest_count = -1
 		path_count = 0
 
+		# For each node in the graph, store it and its connected nodes in  'nodes'
 		for j in range(x, edge_count):
 			curr_node = lines[j].split()[0]
-			# print("Edge: {}".format(lines[j]))
 
 			if curr_node == prev_node:
 				edges.append(lines[j].split()[1])
@@ -73,8 +77,10 @@ def graph_create(line_list, graphs, nodes, edges):
 			
 			prev_node = curr_node
 
+			# Initialize  nodes  values for current node and insert all connect nodes  'edges'
 			nodes[int(lines[j].split()[0])] = [shortest_count, longest_count, path_count], edges
 		
+		# If the last node in the graph is reached, move to the next graph
 		if j == edge_count-1 and i < graph_count-1:
 			node_count = int(lines[j+1])
 			x = edge_count + 2
@@ -86,18 +92,21 @@ def graph_create(line_list, graphs, nodes, edges):
 	return graphs
 
 
-def traverse(nodes, node, path_count, path_length):
+def graph_traverse(nodes, node, path_count, path_length):
 	"""Traverses graphs to find longest/shortest paths and total paths.
 
 	Keyword arguments:
-	node -- A list of nodes
+	nodes -- A list of nodes
+	node  -- A single node from nodes
+	path_count  -- Total amount of paths
+	path_length -- Length of current path
 	"""
 	path_length += 1
 	
 	# For any connected_node, traverse that node unless it's the last node
 	for connected_node in node[1]:
 		try:
-			traverse(nodes, nodes[int(connected_node)], path_count, path_length)
+			graph_traverse(nodes, nodes[int(connected_node)], path_count, path_length)
 			
 		except:
 			# Total paths
@@ -106,55 +115,25 @@ def traverse(nodes, node, path_count, path_length):
 			# Shortest path
 			if path_length < nodes[1][0][0]:
 				nodes[1][0][0] = path_length
-				print("Shortest path: {} ".format(path_length))
 
 			# Longest path
 			if path_length > nodes[1][0][1]:
 				nodes[1][0][1] = path_length
-				print("Longest path: {} ".format(path_length))
 
 			continue
 
 
-def shortest_path(graph):
-	"""Finds the shortest path in a graph
-
-	Keyword arguments:
-	graphs -- A graph list
-	"""
-
-
-def longest_path(graph):
-	"""Finds the longest path in a graph
-
-	Keyword arguments:
-	graph -- A graph list
-	"""
-
-
-def total_paths(graph):
-	"""Finds the total number of paths in a graph.
-
-	Keyword arguments:
-	graph -- A graph list
-	"""
-
 
 if __name__ == "__main__":
-	# file_parse()
-	# print(file_parse())
-	# for graph in file_parse():
 	graphs = file_parse()
-	# count = 0
-	# for graph in graphs 
-	# print(graphs[0][0])
-	# for graph in graphs:
-	traverse(graphs[1][0], graphs[1][0][1], 0, 0, 0)
-	# for graph in graphs:
-		# print(graph[0], graph[0][1])
-	# for graph in (file_parse()):
-		# for nodes in graph:
-	# print(file_parse()[0][0]['1'][0:3])
-	# print(file_parse()[0][0]['1'][4])
-	# print(file_parse()[0][0]['1'][3][1])
-		# shortest_path(graph)
+	
+	i = 1
+	for graph in graphs:
+		graph_traverse(graph[0], graph[0][1], 0, 0)
+
+		print("Graph number: {}".format(i))
+		print("Shortest path: {}".format(graph[0][1][0][0]))
+		print("Longest path: {}".format(graph[0][1][0][1]))
+		print("Number of paths: {}\n".format(graph[0][1][0][2]))
+
+		i += 1
